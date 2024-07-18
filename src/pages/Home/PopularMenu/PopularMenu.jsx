d4344bfca662115e2e787bf08bdf67c1eba74ef8
AminulIslamorange <aminulislamorange@gmail.com>
2024-07-18 18:03:03 +0600

    home page done

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
 
 const PopularMenu = () => {
@@ -12,11 +13,18 @@ const PopularMenu = () => {
             setMenu(popularItem)})
     },[])
     return (
-        <section>
+        <section className="mb-12">
             <SectionTitle subHeading={'---Check it out---'} heading={'FROM OUR MENU'}>
             </SectionTitle>
 
-            <p>{menu.length}</p>
+            <div className="grid md:grid-cols-2 gap-6">
+                {menu.map(item=><MenuItem 
+                key={item._id} 
+                item={item}>
+
+                </MenuItem>)}
+                </div>
+               <div className="text-center"> <button className="btn btn-outline border-0 border-b-4 my-4 ">View Full  Menu</button></div>
 
         </section>
     );
--- src/pages/Shared/MenuItem/MenuItem.jsx
+++ src/pages/Shared/MenuItem/MenuItem.jsx
@@ -1,8 +1,15 @@
 
 
-const MenuItem = () => {
+const MenuItem = ({item}) => {
+    const{price,image,recipe,name}=item;
     return (
-        <div>
+        <div className="flex space-x-4">
+            <img style={{borderRadius:'0px 200px 200px 200px'}} className="w-[120px]" src={image} alt=""/>
+            <div>
+                <h3 className="uppercase">{name}----------</h3>
+                <p>{recipe}</p>
+            </div>
+            <p className="text-[#BB8506]">${price}</p>
             
         </div>
     );
